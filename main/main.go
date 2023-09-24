8e037119bf7907f71b0c688511b70e8f8ce167d8
Matthew Giglio <matthew.giglio@duke.edu>
2023-09-23 23:41:35 -0400

    Fully functional

--- main/main.go
+++ main/main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"fmt"
 	"log"
+	"os"
+	"strconv"
+	"strings"
 )
+
+func getInfo(message string) (string, error) {
+	fmt.Print(message)
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("An error occured while reading input. Please try again", err)
+		return "", fmt.Errorf("An error occured while reading input: %v", err)
+	}
+	input = strings.TrimSuffix(input, "\n")
+	return input, nil
+}
 
 func main() {
 	ctx := context.Background()
@@ -11,7 +28,23 @@ func main() {
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	err = scheduleMeetings(ctx, client)
+	day, err := getInfo("Enter day in YYYY-MM-DD format: ")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	startTime, err := getInfo("Enter starting time in HH:MM:SS format in PT: ")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	sessionLength, err := getInfo("Enter length of session in minutes: ")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	length, err := strconv.Atoi(sessionLength)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	err = scheduleMeetings(ctx, client, day, startTime, length)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
--- main/scheduler.go
+++ main/scheduler.go
@@ -2,44 +2,89 @@ package main
 
 import (
 	"context"
+	"encoding/csv"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
 
 	"github.com/google/uuid"
 	"google.golang.org/api/calendar/v3"
 	"google.golang.org/api/option"
 )
 
-func scheduleMeetings(ctx context.Context, client *http.Client) error {
+func scheduleMeetings(ctx context.Context, client *http.Client, day string, startTime string, duration int) error {
 	srv, err := calendar.NewService(ctx, option.WithHTTPClient(client))
 	if err != nil {
 		return fmt.Errorf("Unable to retrieve Calendar client: %v", err)
 	}
-	event := createEvent()
-	calendarId := "primary"
-	event, err = srv.Events.Insert(calendarId, event).ConferenceDataVersion(1).Do()
+	records, err := readFile()
 	if err != nil {
-		return fmt.Errorf("Unable to create event. %v\n", err)
+		return err
+	}
+	for i := 0; i < len(records); i++ {
+		if i != 0 {
+			event := createEvent(records[i], day, startTime, duration)
+			calendarId := "primary"
+			event, err = srv.Events.Insert(calendarId, event).ConferenceDataVersion(1).Do()
+			if err != nil {
+				return fmt.Errorf("Unable to create event. %v\n", err)
+			}
+		}
 	}
 	return nil
 }
 
-func createEvent() *calendar.Event {
+func readFile() ([][]string, error) {
+	file, err := os.Open("meetings.csv")
+	if err != nil {
+		return nil, fmt.Errorf("Error while reading the file %v", err)
+	}
+	defer file.Close()
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		return nil, fmt.Errorf("Error reading records: %v", err)
+	}
+	return records, nil
+}
+
+func getTimes(round, startTime string, duration int) (string, string) {
+	round_num, err := strconv.Atoi(round)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	t, err := time.Parse("15:04:05", startTime)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	time_to_add := time.Duration(duration*(round_num-1))*time.Minute + time.Duration(round_num-1)*time.Minute
+	start := t.Add(time_to_add)
+	startingTime := start.Format("15:04:05")
+	time_to_add = time.Duration(duration) * time.Minute
+	end := start.Add(time_to_add)
+	endingTime := end.Format("15:04:05")
+	return startingTime, endingTime
+}
+
+func createEvent(record []string, day, startTime string, duration int) *calendar.Event {
+	round, attendee_1, attendee_2 := record[0], record[1], record[2]
+	startingTime, endingTime := getTimes(round, startTime, duration)
 	return &calendar.Event{
-		Summary:     "Google I/O 2015",
-		Location:    "800 Howard St., San Francisco, CA 94103",
-		Description: "A chance to hear more about Google's developer products.",
+		Summary: fmt.Sprintf("FSD Round %s", round),
 		Start: &calendar.EventDateTime{
-			DateTime: "2023-09-28T09:00:00-07:00",
+			DateTime: fmt.Sprintf("%sT%s", day, startingTime),
 			TimeZone: "America/Los_Angeles",
 		},
 		End: &calendar.EventDateTime{
-			DateTime: "2023-09-28T17:00:00-07:00",
+			DateTime: fmt.Sprintf("%sT%s", day, endingTime),
 			TimeZone: "America/Los_Angeles",
 		},
 		Attendees: []*calendar.EventAttendee{
-			{Email: "matthew.giglio10@gmail.com"},
-			{Email: "matthew@joincandidhealth.com"},
+			{Email: attendee_1},
+			{Email: attendee_2},
 		},
 		ConferenceData: &calendar.ConferenceData{
 			CreateRequest: &calendar.CreateConferenceRequest{
